749e474fd343634678314086d18f3b1687ee49d4
AnMankov <tosa1@mail.ru>
2020-05-16 21:31:07 +0300

    lab2-2 the first commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var base =  2; 
 var exp  = 10;
-var ctr  =  0;
 var res  =  1;
 
-while ( ctr++ < exp )
+for ( var ctr = 0; ctr < exp ; ++ctr )
 {
-  res *= base; 
+  res *= base;
 }
 
 console.log( res );
