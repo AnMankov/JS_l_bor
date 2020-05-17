89d4fda4107901d424c45999f1ccc76edb7cb305
AnMankov <tosa1@mail.ru>
2020-05-17 17:42:40 +0300

    this is a for solution

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function cmp( base )
+var a = [ 5, 12 ];
+var b = [];
+
+a[ 99 ] = 7;
+
+for ( var ctr_src = 0, ctr_dest = 0; ctr_src < a.length; ++ctr_src, ++ctr_dest )
 {
-  return function( nbr )
+  if ( a[ ctr_src ] !== undefined )
   {
-    return ( nbr == base ) ? null : base > nbr; 
+    b[ ctr_dest ] = Math.pow( a[ ctr_src ], 2 );
+    console.log( "b[ " + ctr_dest + " ] = ", b[ ctr_dest ] );
+  }
+  else
+  {
+    b[ ctr_dest ] = undefined;
   }
 }
-
-console.log( cmp( 10 )( 9 ) );
-console.log( cmp( 10 )( 10 ) );
-console.log( cmp( 10 )( 11 ) );
