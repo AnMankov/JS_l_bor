4ea1baaa80cc088446c0ec7f14eb9f51906b07a8
AnMankov <tosa1@mail.ru>
2020-05-21 12:11:53 +0300

    ok search

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-var name = "John"; //имя
+var s = "Мы знаем, что монохромный цвет - это градации серого";
+var txt = "хром";
+var word;
 
-console.log("Hello, " + name);
+var slicePos = s.indexOf( txt );
+
+if ( slicePos != -1 )
+{
+  var startPos   = s.lastIndexOf( " ", slicePos ) + 1;
+  var endPos = s.indexOf( " ", startPos );
+  if ( endPos != -1 )
+  {
+    word = s.slice( startPos, endPos );
+  }
+  else
+  {
+    word = s.slice( startPos );
+  }
+    
+  console.log( word );
+}
+else
+{
+  console.log( "The word is absent" );
+}
